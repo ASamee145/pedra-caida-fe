d4f9f826f98f632899d3df8fba011fb2d068b2c4
Muhammad <mujtabakhalid1999@gmail.com>
2026-01-28 15:08:52 +0500

    home page half done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/components/home/home';
+import { MainPage } from './pages/components/main-page/main-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainPage, children: [{ path: '', component: Home }] },
+];
---
+++ src/app/pages/components/main-page/main-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Footer } from '../../../layout/components/footer/footer';
+import { Navbar } from '../../../layout/components/navbar/navbar';
+
+@Component({
+  selector: 'app-main-page',
+  imports: [Navbar, Footer, RouterModule],
+  templateUrl: './main-page.html',
+  styleUrl: './main-page.scss',
+})
+export class MainPage {}
